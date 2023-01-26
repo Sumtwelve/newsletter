bc80f7f72db451a96196da2532367c62623f232b
sumtwelve <sumtwelve@gmail.com>
2023-01-25 23:55:15 -0700

    minor alterations to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
 //TODO: find a way to remove the \n characters from the article content. Might make displaying it look more like a proper madlib
 
 
-//const axios = require('axios');
-const apiKey = "f52ad48a-9631-4f70-bcba-5913be0363a5"
-const url = `https://api.goperigon.com/v1/all?category=Business&sourceGroup=top100&showReprints=false&apiKey=${apiKey}`
+// API call to Perigon's newsfeed API. Gets entire news articles by category.
+function getArticle() {
+    const perigonKey = "f52ad48a-9631-4f70-bcba-5913be0363a5"
+    // TODO: if you want to let the user pick the topic of the article, their choice should probably go in the `?category=` parameter below
+    const perigonUrl = `https://api.goperigon.com/v1/all?category=Business&sourceGroup=top100&showReprints=false&apiKey=${perigonKey}`
 
-fetch (url)
-.then(function(response) {
-    if (!response.ok) {
-        throw response.json();
-    }
-    return response.json();
-})
-.then(function(results) {
-    console.log(results);
-    printContent(results);
-});
-
-function printContent(data) {
-    
+    fetch (perigonUrl)
+    .then(function(response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+        return response.json();
+    })
+    .then(function(results) {
+        console.log(results);
+        // TODO: call function here that deals with the article content
+    });
 }
+
+
